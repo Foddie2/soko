5fce37a7ca4616ffc20316af16f9fac7a743a06b
Foddie2 <Bensonagala@live.com>
2022-08-08 11:26:18 +0300

    tailwind config

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-3xl font-bold">Next tailwind Soko</h1>
+      <h1 className="text-3xl font-bold underline">Next tailwind Soko</h1>
     </div>
   );
 }
